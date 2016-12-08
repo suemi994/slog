c9723ac68bb1033d8bb3c1c4ea961d4d3ac1019d
suemi <suemi94@qq.com>
2016-12-08 21:59:26 +0800

    增加了LogScheduler的声明

--- include/slog/logging/log_scheduler.h
+++ include/slog/logging/log_scheduler.h
@@ -5,13 +5,63 @@
 #ifndef SLOG_LOG_SCHEDULER_H
 #define SLOG_LOG_SCHEDULER_H
 
+#include <vector>
+#include <thread>
+#include <condition_variable>
 #include "slog/utils/no_copyable.h"
+#include "slog/base/fixed_buffer.h"
 
 namespace slog {
 
+class Appender;
+
+/**
+ * 集中处理所有的日志写入工作，委托各个Appender实际输出到各个目的输出
+ * @author suemi
+ */
 class LogScheduler : public NoCopyable {
+  using AppenderList = std::vector<std::shared_ptr<Appender>>;
+  using Buffer = FixedBuffer<detail::LARGE_BUFFER_SIZE>;
+  using BufferPtr = std::unique_ptr<Buffer>;
+  using BufferList = std::vector<BufferPtr>;
 public:
-  LogScheduler(int fulsh_interval);
+  LogScheduler(int flush_interval);
+  ~LogScheduler();
+
+  void Append(const char* buf, int len);
+
+  void Flush();
+
+  void Start();
+
+  void Stop();
+
+  void AddAppender(std::shared_ptr<Appender> appender_ptr, bool can_delay = true);
+
+  void RemoveAllAppenders();
+
+  void RemoveAppender(std::shared_ptr<Appender> appender_ptr);
+
+  void RemoveAppender(const std::string& appender_name);
+
+  bool is_running() const;
+
+private:
+
+  void DaemonFunc();
+
+  AppenderList delay_appenders_;
+  AppenderList immediate_appenders_;
+
+  bool is_running_;
+  const int flush_interval_;
+  std::thread thread_;
+  std::condition_variable cond_;
+
+  BufferPtr current_buffer_;
+  BufferPtr next_buffer_;
+  BufferList buffers_;
+  BufferList buffers_to_write_;
 };
 
 }
