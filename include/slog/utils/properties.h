ba519e6b3d3c4901b2782d003138a28013039615
suemi <suemi94@qq.com>
2016-12-09 09:44:30 +0800

    增加Properties的声明

--- include/slog/utils/properties.h
+++ include/slog/utils/properties.h
@@ -5,6 +5,11 @@
 #ifndef SLOG_PROPERTIES_H
 #define SLOG_PROPERTIES_H
 
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 namespace slog {
 
 /**
@@ -12,6 +17,50 @@ namespace slog {
  * @author suemi
  */
 class Properties {
+  using StringMap = std::unordered_map<std::string,std::string>;
+public:
+  Properties();
+
+  explicit Properties(std::istream &in);
+
+  explicit Properties(const std::string &input_file);
+
+  bool Exists(const std::string &key) const;
+
+  bool Exists(const char *key) const;
+
+  int Size() const;
+
+  const std::string &GetProperty(const std::string &key) const;
+
+  const std::string &GetProperty(const char *key) const;
+
+  const std::string &GetProperty(const std::string &key, const std::string &default_value) const;
+
+  std::vector<std::string> PropertyNames() const;
+
+  void SetProperty(const std::string &key, const std::string &value);
+
+  void RemoveProperty(const std::string &key);
+
+  Properties GetPropertySubset(const std::string &prefix) const;
+
+  bool GetInt(int &val, const std::string &key) const;
+
+  bool GetBool(bool &val, const std::string &key) const;
+
+  bool GetLong(long &val, const std::string &key) const;
+
+  bool GetFloat(float& val, const std::string& key) const ;
+
+private:
+
+  void Initialize(std::istream& in);
+
+  template<typename T>
+  bool GetValue(T& val, const std::string& key) const;
+
+  StringMap values_;
 
 };
 
